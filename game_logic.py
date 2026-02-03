1e4ce165879d107884ace9e2a501ff4d98996944
Ilari <ilari22000@student.hamk.fi>
2026-02-03 17:16:39 +0200

    split game logic started heuristics

---
+++ game_logic.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import random
+
+df = pd.read_csv('wordle.csv')
+df.drop(['occurrence', 'day'], axis=1, inplace=True)
+#print(df)
+
+correct_word = df['word'].str.upper().tolist()
+#correct_word = df['word'].sample(1).iloc[0].upper()
+
+current_row = 0
+
+def feedback(guess, answer):
+    guess = guess.upper()
+    answer = answer.upper()
+
+    result = ['gray']*len(guess)
+    answer_chars = list(answer)
+
+    for i in range(len(guess)):
+        if guess[i] == answer[i]:
+            result[i] = "green"
+            answer_chars[i] = None
+
+    for i in range(len(guess)):
+        if result[i] == "green":
+            continue
+        if guess[i] in answer_chars:
+            result[i] = "yellow"
+            answer_chars[answer_chars.index(guess[i])] = None
+    return result
+
+def single_game(answer,wordlist,strategy):
+    candidates = wordlist.copy()
+    guesses = []
+
+    for _ in range(6):
+        guess = strategy(candidates)
+        guesses.append(guess)
+
+        fb = feedback(guess, answer)
+
+        if guess == answer:
+            return True, guesses
+        
+        candidates = [w for w in candidates if feedback(guess,w) == fb]
+    return False, guesses
